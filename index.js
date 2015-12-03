f8ee44d6ff6f13c0c3984188876d4e898f16a511
Joshua Johnston <josh@162.235.21.198.tigernet.wifi.dyn.clemson.edu>
2015-12-03 00:08:40 -0500

    Remove cool

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  var result = ''
-  var times = process.env.TIMES || 5
-  for (i=0; i < times; i++)
-    result += cool();
   response.render('pages/index')
 });
 
